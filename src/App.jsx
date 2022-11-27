117790d373d11486456a87cf570d36934570094a
Krishna Mahato <krishhtrishh9304@gmail.com>
2022-11-27 17:03:52 +0530

    ADD🚀: Initial frontend setup react + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="w-screen h-screen bg-red-500">Hello sexy</div>;
+}
+
+export default App;
