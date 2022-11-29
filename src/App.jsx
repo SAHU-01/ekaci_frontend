630e8cc050991c1bc4020b35fa647d99051fb4ce
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2022-11-29 20:39:09 +0530

    dashboard, sidenav added in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
-import Register from "./components/Register";
+import Register from "./pages/Register";
 import Farmer_Register from "./pages/Farmer_Register";
 import Insurance_Registeration from "./pages/Insurance_Registeration";
 import Government_Registeration from "./pages/Government_Register";
+import Farmer_Dashboard from "./pages/Farmer_Dashboard";
 
 function App() {
   const activeMenu = false;
@@ -21,6 +22,7 @@ function App() {
           path="/government_register"
           element={<Government_Registeration />}
         />
+        <Route path="/farmer_dashboard" element={<Farmer_Dashboard />} />
       </Routes>
     </>
   );
