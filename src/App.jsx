0fea4b69fc2b488782486a3e4d7e0f2073df6002
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2022-11-29 15:01:05 +0530

    farmer registeration form ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
+import Farmer_Register from "./pages/Farmer_Register";
 
 function App() {
   const activeMenu = false;
@@ -9,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/farmer_register" element={<Farmer_Register />} />
       </Routes>
     </>
   );
